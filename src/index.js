9a01a2b2249fa86cf8ba20d0c43d1f8754ea04ec
Elturanio <82751462+Elturanio@users.noreply.github.com>
2022-07-18 12:11:38 +0600

    I deleted redux-devtools

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ import { rootReducer } from './redux/rootReducer';
 import App from './components/App';
 
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
